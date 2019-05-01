56eab0ad3ed0ddfff0263c8966a1ad3f162b536c
dolmengate <stephenjroman@protonmail.com>
2019-04-30 23:08:53 -0400

    add re-establish hardlinks for user configs, fix repo location in ansible_install.sh

--- install_ansible.sh
+++ install_ansible.sh
@@ -5,4 +5,6 @@ sudo apt-get install software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt-get install ansible -y
 
-cp ./roles/common/files/etc/ansible/facts.d/ /etc/ansible/
+mkdir /etc/ansible/facts.d/
+repo_loc=$(readlink -f "$0")
+cp "$repo_loc"/roles/common/files/etc/ansible/facts.d/ /etc/ansible/
