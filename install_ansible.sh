de70aa5bd23ee882b69648ffc5f8207a2d657e0d
dolmengate <stephenjroman@protonmail.com>
2019-09-22 15:42:09 -0400

    remove docker role for now

--- install_ansible.sh
+++ install_ansible.sh
@@ -4,7 +4,3 @@ sudo apt-get update
 sudo apt-get install software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt-get install ansible -y
-
-mkdir -p /etc/ansible/facts.d/
-repo_loc=$(readlink -f $(dirname "$0"))
-cp -r "$repo_loc"/roles/common/files/etc/ansible/facts.d/ /etc/ansible/
